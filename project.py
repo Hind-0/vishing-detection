cb4306ba7c404b710108c9a1fea0bcbe76de7cc7
Hind-0 <hindmichaal9@gmail.com>
2026-09-04 18:27:00 +0100

    project.py

--- project.py
+++ project.py
@@ -1,7 +1,6 @@
 import pandas as pd
-
+import shap
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.metrics import ConfusionMatrixDisplay
@@ -9,6 +8,7 @@ import matplotlib.pyplot as plt
 import dataframe_image as dfi # pip 3 to install in termina
 from tensorflow import keras
 
+#----Step 1: Data extraction----
 
 train_data=pd.read_csv("composite_train.csv")
 test_data = pd.read_csv("composite_test.csv")
@@ -23,15 +23,15 @@ print("Testing dataframe")
 print(test_data.head())
 print(test_data.info())
 
+
+#----Step 2: Data Cleaning-----
+
 print("Missing values checker for both csv files")
 print(test_data.isnull().sum())
 print(train_data.isnull().sum())
 
 print("Duplicated rows:", train_data.duplicated().sum())
 print("Duplicated rows:", test_data.duplicated().sum())
-
-print(train_data.columns.tolist()) #finds the column name to use in the next step
-print(test_data.columns.tolist())
 
 #deletes any missing values
 df_train=train_data.dropna()
@@ -49,58 +49,20 @@ X_test=df_test['text']
 y_test=df_test['label']
 
 
-#vectorize for the text columns to become numerical ones
+#----Setp 3:Text to numerical data --------
+
 vectorizer=TfidfVectorizer()
 X_train=vectorizer.fit_transform(X_train) #learns the vocabulary and converts text into numbers
 X_test=vectorizer.transform(X_test)
 
+#-----Step 4:Regression model----
+
 model=LogisticRegression()
 model.fit(X_train,y_train)
-pred_A = model.predict(X_test)
 
-# shows the words which associate with vishing and non vishing predictions
-print('start explainability')
-feature_names = vectorizer.get_feature_names_out() #vectorizer  converts text into numerical features/gets word out ex('amazing..)
-coef =model.coef_[0]#gets classifier coefficient/ the larger the value the stronger the influence 
-
-coef_df = pd.DataFrame({"word": feature_names, 
-                        "coefficient": coef
-                        }) #creating a dataframe with words and their coef so positive or negative 
-
-top_terms_vishing = (
-    coef_df  # use ascending=false 
-    .sort_values("coefficient",ascending=False) #use sort to give back the words with the strongest coefficient not just the rows 
-    .head(15) #15 words with strongest coefficient
-)
-
-print("Words most associated with Vishing:")
-print(top_terms_vishing)
-
-top_non_vishing = (
-    coef_df
-    .sort_values("coefficient",ascending=True)
-    .head(15)
-)
-
-
-print("Words most associated with Non-Vishing:")
-print(top_non_vishing)
-
-
-#shows the predicted and actual results and compares both 
-results = pd.DataFrame({
-    "Actual": y_test.values,
-    "Predicted": pred_A
-})
-results["Correct"] = results["Actual"] == results["Predicted"]
-
-final_table = results[["Actual", "Predicted", "Correct"]].head(30).style.hide(axis='index')
-
-# 3. Save to image
-dfi.export(final_table, 'selected_rows.png')
-
-print("Accuracy:", accuracy_score(y_test, pred_A))
-print(classification_report(y_test, pred_A))
+pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, pred))
+print(classification_report(y_test, pred))
 
 
 #confusion matrix
@@ -115,12 +77,12 @@ def plot_cm(y_true, y_pred, title):
     plt.savefig(title)
     plt.show()
 
-plot_cm(y_test, pred_A, "Logistic Regression-Confusion Matrix")
+plot_cm(y_test, pred, "Logistic Regression-Confusion Matrix")
 
 
 
 
-#model- MLP 
+#-----Model 2- MLP--------
 model_A= keras.models.Sequential([ 
 keras.layers.Dense(50, activation="relu",input_shape=X_train.shape[1:]),
 keras.layers.Dense(20, activation="relu"),
@@ -152,12 +114,59 @@ print(classification_report(y_test,binary_MLP))
 
 plot_cm(y_test, binary_MLP, "Model A – Confusion Matrix")
 
-plt.figure(figsize = (15,8))
-plt.title("MLP Predicted VS actuall vihing ")
-plt.plot(binary_MLP,'r-', label='Predicted')
-plt.plot(y_test.values, 'g--', label='Actual')
-plt.xlabel("Sample index")
-plt.ylabel("Predicted vishing (0=non-vishing, Pass=vishing)")
-plt.legend()
-plt.savefig("Model_A.png")
+
+#---Words associated with Vishing and Non-vishing----
+
+feature_names = vectorizer.get_feature_names_out() #vectorizer  converts text into numerical features/gets word out ex('amazing..)
+coef =model.coef_[0]#gets classifier coefficient/ the larger the value the stronger the influence 
+
+coef_df = pd.DataFrame({"word": feature_names, "coefficient": coef}) #creating a dataframe with words and their coef so positive or negative 
+
+top_terms_vishing = (
+    coef_df  # use ascending=false 
+    .sort_values("coefficient",ascending=False) #use sort to give back the words with the strongest coefficient not just the rows 
+    .head(15) #15 words with strongest coefficient
+)
+
+print("Words most associated with Vishing:")
+print(top_terms_vishing)
+
+top_non_vishing = (
+    coef_df
+    .sort_values("coefficient",ascending=True)
+    .head(15)
+)
+
+
+print("Words most associated with Non-Vishing:")
+print(top_non_vishing)
+
+
+#-------Shap-------
+explainer=shap.LinearExplainer(model,X_train) 
+shap_values=explainer(X_test[0:100])
+shap_values.feature_names = vectorizer.get_feature_names_out()
+shap.plots.waterfall(shap_values[0])
 plt.show()
+shap.plots.beeswarm(shap_values, max_display=20)
+plt.show()
+
+#---Compares predictions from both models----
+results = pd.DataFrame({
+    "Actual": y_test.values,
+    "Predicted Model 1": pred,
+    "Predicted Model 2":binary_MLP
+
+})
+results["Correct Model 1"] = (results["Actual"] == results["Predicted Model 1"])
+results["Correct Model 2"] = (results["Actual"] == results["Predicted Model 2"])
+
+final_table = results[[
+    "Actual", 
+    "Predicted Model 1",
+    "Predicted Model 2"
+    "Correct Model 1,"
+    "Correct model 2"]].head(30).style.hide(axis='index')
+
+# 3. Save to image
+dfi.export(final_table, 'Model_comparison.png')
